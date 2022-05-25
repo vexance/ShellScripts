9a8b384d1a13aa6e4ce8294f3bb984058933f84b
Vexance <vex@hades.ohitskilabs.org>
2022-05-25 11:13:49 -0400

    azure tenant ID enum script & segmentation scanning helper

---
+++ get-azure-tenant.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [[ $1 == '--help' || $1 == '-h' || -z $1 ]]; then
+    echo "Attempt to identify a domain's azure tenant Id"
+    echo "  Example: ./get-azure-tenant.sh <domain.tld>"
+    exit
+fi
+
+curl -s "https://login.microsoftonline.com/$1/v2.0/.well-known/openid-configuration" | jq
